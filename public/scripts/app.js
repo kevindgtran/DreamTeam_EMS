9ca63523589cd49e1b0e4b3d21f4d782440b6fb5
ktdodd511 <katiedodd@Katies-MacBook-Air.local>
2016-11-21 17:04:31 -0800

    merge fun

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
+
 var employees;
-=======
-var allEmployees = [];
-  var employees;
->>>>>>> 2852e1f8615d5297dfc26a760c50ee695288dc4b
+
 
 
 $(document).ready(function() {
@@ -12,11 +9,7 @@ $(document).ready(function() {
 
   $.ajax({
       method: 'GET',
-<<<<<<< HEAD
       url: '/api/companies',
-=======
-      url: 'http://localhost:3000/api/companies',
->>>>>>> 2852e1f8615d5297dfc26a760c50ee695288dc4b
       dataType: 'json',
       success: onSuccess
   });
@@ -35,10 +28,6 @@ $(document).ready(function() {
 
   $('#createButton').on('click', function(e) {
       e.preventDefault();
-<<<<<<< HEAD
-=======
-      console.log('New employee created', $(this).serialize());
->>>>>>> 2852e1f8615d5297dfc26a760c50ee695288dc4b
       $.ajax({
         method: 'POST',
         url: '/api/companies/:companyId/employees',
@@ -46,10 +35,7 @@ $(document).ready(function() {
         success: newEmployee,
         error: newEmployeeError
       });
-<<<<<<< HEAD
-=======
-      console.log('button works');
->>>>>>> 2852e1f8615d5297dfc26a760c50ee695288dc4b
+
   });
 
   function newEmployee(json) {
@@ -58,11 +44,7 @@ $(document).ready(function() {
   }
 
   function newEmployeeError() {
-<<<<<<< HEAD
     console.log("ERROR: " + "new employee not created");
-=======
-    console.log("error: " + "new employee not created");
->>>>>>> 2852e1f8615d5297dfc26a760c50ee695288dc4b
   }
 
 });
