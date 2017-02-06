9515f81a14eb34c3c4abc3e3e00f59ba5e066752
Kevin Tran <kevin2005tran@gmail.com>
2017-02-05 21:40:20 -0800

    created signup route view and logic

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 
+  var signupData = $("#signup-form").serialize();
+  console.log(signupData);
+
+  $.post('/users', signupData, function(response){
+    console.log(response);
+  })
+
+
+
+
+
+
+
 
 
 });
--- server.js
+++ server.js
@@ -5,19 +5,38 @@ var express = require('express'),
     db = require('./models'),
     controllers = require('./controllers'),
     app = express();
+var User = require('./models/user');
 
 // middleware
 app.use(cors());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
+app.use(bodyParser.urlencoded({extended: true}));
+// mongoose.connect('mongodb://localhost/Project-1');
 
 /***********
  * ROUTES *
  ***********/
+
+app.get('/signup', function (req, res) {
+  res.render('signup');
+});
+
+app.post('/users', function (req, res) {
+  User.createSecure(req.body.email, req.body.password, function (err, user) {
+    res.json(user);
+  });
+});
+
+
+app.get('/login', function (req, res) {
+  res.send('login coming soon!');
+});
+
+
+
+
+
 
 // get index.html
 app.get('/', function(req, res) {
@@ -36,7 +55,7 @@ app.delete('/api/companies/:companyId/employees', controllers.employee.destroy);
 /***********
  * SERVER *
  ***********/
- 
+
 app.listen(process.env.PORT || 3000, function() {
     console.log('listening to http://localhost:3000/');
 });
